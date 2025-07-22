99fee90929df9304206d471236a63efb21b25ef7
Olena Mustafina <toysamiy.mail@gmail.com>
2025-07-22 15:43:23 +0300

    feat: Add functionality to remove products from Cart

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -28,7 +28,9 @@ const Cart = () => {
                   <p>${item.price}</p>
                   <p>{cartItems[item._id]}</p>
                   <p>${item.price * cartItems[item._id]}</p>
-                  <p>x</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    x
+                  </p>
                 </div>
                 <hr />
               </div>
